e591b27d7aaa7a2e271c73b25fd9e0e1290ad07c
Polushin Maksim <polushinmc@gmail.com>
2022-08-17 01:20:42 +0400

    Add logging.

--- web_demo_cocktails/logger.py
+++ web_demo_cocktails/logger.py
@@ -9,16 +9,16 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
 
-    str_format = "%(asctime)s %(message)s"
-
     reset = "\x1b[0m"
 
+    format = "%(asctime)s %(message)s"
+
     FORMATS = {
-        logging.DEBUG: grey + str_format + reset,
-        logging.INFO: white + str_format + reset,
-        logging.WARNING: yellow + str_format + reset,
-        logging.ERROR: red + str_format + reset,
-        logging.CRITICAL: bold_red + str_format + reset
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: white + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
     }
 
     def format(self, record):
@@ -29,7 +29,7 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
-class CustomLogger(logging.Logger):
+class CustomLogger:
     levels = {'CRITICAL': 50, 'FATAL': 50, 'Fatal': 50, 'fatal': 50, 1: 50,
               'ERROR': 40, 'Error': 40, 'error': 40, 2: 40, False: 40,
               'WARNING': 30, 'WARN': 30, 'Warning': 30, 'warning': 30, 3: 30,
